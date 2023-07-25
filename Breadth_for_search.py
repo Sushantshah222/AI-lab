7f9303d189139b3ea2de20b3846bca53ec6dfb7a
sushant shah <sushant.shah222@gmail.com>
2023-07-26 01:21:29 +0545

    update repo

--- Breadth_for_search.py
+++ Breadth_for_search.py
@@ -13,32 +13,55 @@
 #     'H' : ['E']
 # }
 
-G = {
-'A' : ['B', 'C'],
-'B' : ['A', 'D', 'E'],
-'C' : ['A', 'F'],
-'D' : ['B', 'G'],
-'E' : ['B', 'D', 'G', 'H'],
-'F' : ['C'],
-'G' : ['D', 'E'],
-'H' : ['E']
+# G = {
+# 'A' : ['B', 'C'],
+# 'B' : ['A', 'D', 'E'],
+# 'C' : ['A', 'F'],
+# 'D' : ['B', 'G'],
+# 'E' : ['B', 'D', 'G', 'H'],
+# 'F' : ['C'],
+# 'G' : ['D', 'E'],
+# 'H' : ['E']
+# }
+
+# for key,val in G.items():
+#     print(f"{key} --> {val}")
+
+
+# start = 'A'
+# goal = 'H'
+
+# def DFS(G,start,goal):
+#     stack = [start]
+#     visited = set()
+#     previousV = dict()
+#     previousV[start]=''
+
+#     while stack:
+#         popedVertex = stack.pop()
+#         if popedVertex not in visited:
+#             if popedVertex == goal:
+            
+
+# Using a Python dictionary to act as an adjacency list
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
 }
 
-for key,val in G.items():
-    print(f"{key} --> {val}")
+visited = set() # Set to keep track of visited nodes of graph.
 
+def dfs(visited, graph, node):  #function for dfs 
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
 
-start = 'A'
-goal = 'H'
-
-def DFS(G,start,goal):
-    stack = [start]
-    visited = set()
-    previousV = dict()
-    previousV[start]=''
-
-    while stack:
-        popedVertex = stack.pop()
-        if popedVertex not in visited:
-            if popedVertex == goal:
-            
+# Driver Code
+print("Following is the Depth-First Search")
+dfs(visited, graph, '5')
